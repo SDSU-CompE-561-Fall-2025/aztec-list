f19e7065c1ae51510e6426569df088a09625dab4
husain <h.y.patanwala@gmail.com>
2025-10-20 11:06:25 -0700

    fix seller_id && further define listing schemas

--- src/app/models/listing.py
+++ src/app/models/listing.py
@@ -17,13 +17,13 @@ class Listing(Base):
     __tablename__ = "listings"
 
     id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, default=uuid.uuid4, index=True)
-    seller_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"), unique=True, index=True)
+    seller_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"), index=True)
     title: Mapped[str] = mapped_column(String)
     description: Mapped[str] = mapped_column(String)
     price: Mapped[float] = mapped_column(float, default=0.0)
     category: Mapped[Category] = mapped_column(Enum(Category))
     condition: Mapped[Condition] = mapped_column(Enum(Condition))
-    is_active: Mapped[bool] = mapped_column(bool)
+    is_active: Mapped[bool] = mapped_column(bool, default=False)
     thumbnail_url: Mapped[str | None] = mapped_column(String)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), default=lambda: datetime.now(UTC)
--- src/app/schemas/listing.py
+++ src/app/schemas/listing.py
@@ -1,18 +1,38 @@
-from pydantic import BaseModel
+import uuid
+from datetime import datetime
+
+from pydantic import BaseModel, Field, HttpUrl
+
+from app.core.database import Category, Condition
 
 
 class ListingBase(BaseModel):
-    title: str
-    price: float
+    title: str = Field(max_length=50)
+    price: float = Field(ge=0.0)
+    category: Category
+    condition: Condition
+    is_active: bool
 
 
 class ListingCreate(ListingBase):
     description: str
+    thumbnail_url: HttpUrl | None = None
 
 
 class ListingUpdate(BaseModel):
-    pass
+    title: str | None = Field(None, max_length=50)
+    description: str | None = Field(None)
+    price: float | None = Field(ge=0.0)
+    category: Category | None
+    condition: Condition | None
+    thumbnail_url: HttpUrl | None
+    is_active: bool
 
 
 class ListingPublic(ListingBase):
-    pass
+    id: uuid
+    seller_id: uuid
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = {"from_attributes": True}
