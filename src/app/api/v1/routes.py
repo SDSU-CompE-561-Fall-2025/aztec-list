efead0707b66181b90901ee2a3721a83077d2eb8
Zander Barajas <zanderbr@protonmail.com>
2025-10-20 01:38:03 -0700

    Implement Profile endpoints

--- src/app/api/v1/routes.py
+++ src/app/api/v1/routes.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from app.routes.auth import auth_router
+from app.routes.profiles import profile_router
 from app.routes.users import user_router
 
 api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(auth_router)
 api_router.include_router(user_router)
+api_router.include_router(profile_router)
