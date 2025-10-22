898030298fd4008ee2c3ff4be2b4838c07f590b2
husain <h.y.patanwala@gmail.com>
2025-10-22 12:42:08 -0700

    update listing routes w/ basic functionality

--- src/app/routes/listing.py
+++ src/app/routes/listing.py
@@ -1,20 +1,20 @@
 import uuid
-from typing import Annotated  # noqa: F401
+from typing import Annotated
 
-from fastapi import APIRouter, Depends  # noqa: F401
-from sqlalchemy.orm import Session  # noqa: F401
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-from app.core.database import get_db  # noqa: F401
+from app.core.database import get_db
 from app.core.dependencies import get_current_user, get_current_user_id  # noqa: F401
 from app.models.listing import Listing
 from app.schemas.listing import (
-    ListingCreate,  # noqa: F401
+    ListingCreate,
     ListingPublic,  # noqa: F401
     ListingSearchParams,  # noqa: F401
     ListingSearchResponse,  # noqa: F401
-    ListingUpdate,  # noqa: F401
+    ListingUpdate,
 )
-from app.services.listing import listing_service  # noqa: F401
+from app.services.listing import listing_service
 
 listing_router = APIRouter(
     prefix="/listings",
@@ -27,26 +27,36 @@ async def get_listings() -> list["Listing"]:
     pass
 
 
-@listing_router.post("/")
-async def create_listing() -> Listing:
+@listing_router.get("{user_id}/")
+async def get_user_listings(user_id: uuid.UUID) -> list["Listing"]:
     pass
 
 
 @listing_router.get("/{listing_id}")
-async def get_listing_by_id(listing_id: uuid.UUID) -> Listing:
-    pass
+async def get_listing_by_id(
+    db: Annotated[Session, Depends(get_db)], listing_id: uuid.UUID
+) -> Listing:
+    return listing_service.get_by_id(db, listing_id)
+
+
+@listing_router.post("/")
+async def create_listing(
+    db: Annotated[Session, Depends(get_db)],
+    seller_id: Annotated[uuid.UUID, Depends(get_current_user_id)],
+    listing: ListingCreate,
+) -> Listing:
+    return listing_service.create(db, seller_id, listing)
 
 
 @listing_router.patch("/{listing_id}")
-async def update_listing_by_id(listing_id: uuid.UUID) -> Listing:
-    pass
+async def update_listing_by_id(
+    db: Annotated[Session, Depends(get_db)], listing_id: uuid.UUID, listing: ListingUpdate
+) -> Listing:
+    return listing_service.update(db, listing_id, listing)
 
 
 @listing_router.delete("/{listing_id}")
-async def delete_listing_by_id(listing_id: uuid.UUID) -> Listing:
-    pass
-
-
-@listing_router.get("{user_id}/")
-async def get_user_listings(user_id: uuid.UUID) -> list["Listing"]:
-    pass
+async def delete_listing_by_id(
+    db: Annotated[Session, Depends(get_db)], listing_id: uuid.UUID
+) -> Listing:
+    return listing_service.delete(db, listing_id)
