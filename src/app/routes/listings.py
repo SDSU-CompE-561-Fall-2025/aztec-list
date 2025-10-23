5797f9698a793c736f72593dfc286f74766a3800
husain <h.y.patanwala@gmail.com>
2025-10-23 11:34:52 -0700

    add listing/{listing_id} route method

--- src/app/routes/listings.py
+++ src/app/routes/listings.py
@@ -1,12 +1,29 @@
-from fastapi import APIRouter
+import uuid
+from typing import Annotated
 
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from app.core.database import get_db
 from app.core.dependencies import get_current_user, get_current_user_id  # noqa: F401
 from app.schemas.listing import (
+    ListingPublic,
     ListingSearchParams,  # noqa: F401
     ListingSearchResponse,  # noqa: F401
 )
+from app.services.listing import listing_service
 
 listing_router = APIRouter(
     prefix="/listings",
     tags=["Listing"],
 )
+
+
+@listing_router.get(
+    "/{listing_id}",
+    status_code=status.HTTP_200_OK,
+)
+async def get_listing_by_id(
+    db: Annotated[Session, Depends(get_db)], listing_id: uuid.UUID
+) -> ListingPublic:
+    return listing_service.get_by_id(db, listing_id)
