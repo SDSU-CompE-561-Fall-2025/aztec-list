f656af0cbdc385b2e2c9bf55d9e7f087aacf3cb7
husain <h.y.patanwala@gmail.com>
2025-10-23 11:50:20 -0700

    add working endpoints -> create listing and get by listing id

--- src/app/routes/listings.py
+++ src/app/routes/listings.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.core.dependencies import get_current_user, get_current_user_id  # noqa: F401
 from app.schemas.listing import (
+    ListingCreate,
     ListingPublic,
     ListingSearchParams,  # noqa: F401
     ListingSearchResponse,  # noqa: F401
@@ -27,3 +28,12 @@ async def get_listing_by_id(
     db: Annotated[Session, Depends(get_db)], listing_id: uuid.UUID
 ) -> ListingPublic:
     return listing_service.get_by_id(db, listing_id)
+
+
+@listing_router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_listing(
+    db: Annotated[Session, Depends(get_db)],
+    seller_id: Annotated[uuid.UUID, Depends(get_current_user_id)],
+    listing: ListingCreate,
+) -> ListingPublic:
+    return listing_service.create(db, seller_id, listing)
