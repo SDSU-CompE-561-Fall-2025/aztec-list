071291185cafb173559f7f8070ecf56cc0626e1f
husain <h.y.patanwala@gmail.com>
2025-10-24 12:32:10 -0700

    add the nested listing route into user routes
    In order to adhere to our initial plan and documentation, the get listings for a user route is placed under USERS instead of LISTING
    
    It is done this way to maintain the same path and avoid problem like circular imports

--- src/app/routes/users.py
+++ src/app/routes/users.py
@@ -5,8 +5,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
+from app.models.listing import Listing
 from app.models.user import User
+from app.schemas.listing import ListingPublic, UserListingsParams
 from app.schemas.user import UserPublic
+from app.services.listing import listing_service
 from app.services.user import user_service
 
 user_router = APIRouter(
@@ -34,3 +37,14 @@ async def get_user(
         HTTPException: 404 if user not found, 500 on database error
     """
     return user_service.get_by_id(db, user_id)
+
+
+@user_router.get(
+    "/{user_id}/listings", summary="Get user's listings", response_model=list[ListingPublic]
+)
+async def get_user_listings(
+    db: Annotated[Session, Depends(get_db)],
+    user_id: uuid.UUID,
+    params: Annotated[UserListingsParams, Depends()],
+) -> list[Listing]:
+    return listing_service.get_by_seller(db, user_id, params)
