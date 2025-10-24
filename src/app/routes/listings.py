adeb9e96db71045a4f33363ee7895b201fd8d331
Zander Barajas <131927163+xZanderBR@users.noreply.github.com>
2025-10-24 03:34:18 -0700

    Fix type errors

--- src/app/routes/listings.py
+++ src/app/routes/listings.py
@@ -5,11 +5,11 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
-from app.core.dependencies import get_current_user, get_current_user_id  # noqa: F401
+from app.core.dependencies import get_current_user_id
+from app.models import Listing
 from app.schemas.listing import (
     ListingCreate,
     ListingPublic,
-    ListingSearchParams,  # noqa: F401
     ListingUpdate,
 )
 from app.services.listing import listing_service
@@ -20,29 +20,30 @@ listing_router = APIRouter(
 )
 
 
-@listing_router.post("/", status_code=status.HTTP_201_CREATED)
+@listing_router.post("/", response_model=ListingPublic, status_code=status.HTTP_201_CREATED)
 async def create_listing(
     db: Annotated[Session, Depends(get_db)],
     seller_id: Annotated[uuid.UUID, Depends(get_current_user_id)],
     listing: ListingCreate,
-) -> ListingPublic:
+) -> Listing:
     return listing_service.create(db, seller_id, listing)
 
 
 @listing_router.get(
     "/{listing_id}",
+    response_model=ListingPublic,
     status_code=status.HTTP_200_OK,
 )
 async def get_listing_by_id(
     db: Annotated[Session, Depends(get_db)], listing_id: uuid.UUID
-) -> ListingPublic:
+) -> Listing:
     return listing_service.get_by_id(db, listing_id)
 
 
-@listing_router.patch("/{listing_id}", status_code=status.HTTP_200_OK)
+@listing_router.patch("/{listing_id}", response_model=ListingPublic, status_code=status.HTTP_200_OK)
 async def update_listing(
     db: Annotated[Session, Depends(get_db)], listing_id: uuid.UUID, listing: ListingUpdate
-) -> ListingPublic:
+) -> Listing:
     return listing_service.update(db, listing_id, listing)
 
 
