727b726bd27cce55947f1aae1a8d30f48b237b5a
Antonio Hengel <antoniohengel7@gmail.com>
2025-10-25 21:44:29 -0700

    update routes.py to include listing_images

--- src/app/api/v1/routes.py
+++ src/app/api/v1/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.routes.auth import auth_router
+from app.routes.listing_images import listing_images_router
 from app.routes.listings import listing_router
 from app.routes.profiles import profile_router
 from app.routes.users import user_router
@@ -11,3 +12,4 @@ api_router.include_router(auth_router)
 api_router.include_router(user_router)
 api_router.include_router(profile_router)
 api_router.include_router(listing_router)
+api_router.include_router(listing_images_router)
