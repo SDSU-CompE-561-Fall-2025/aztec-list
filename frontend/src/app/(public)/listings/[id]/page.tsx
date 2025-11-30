90324fdf8be3cf6c5a956cd8c662a26289ba58e2
husain <h.y.patanwala@gmail.com>
2025-11-29 20:33:01 -0800

    rework folder structure to include src && update (public) route structure

---
+++ frontend/src/app/(public)/listings/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <p>Get specific listing details + page</p>
+  );
+}
---
+++ frontend/src/app/(public)/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <p>AztecList landing page</p>
+  );
+}
