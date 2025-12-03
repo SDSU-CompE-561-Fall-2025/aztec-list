704e82c6249c6890ed7573f71c2aaaa6cb5d3721
husain <h.y.patanwala@gmail.com>
2025-12-03 07:19:02 -0800

    add prototype data fetching

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Grid } from "@/components/ui/Grid";
+
 export default function Home() {
   return (
     <>
       some temp text
+      <Grid></Grid>
     </>
   );
 }
--- frontend/src/components/ui/Grid.tsx
+++ frontend/src/components/ui/Grid.tsx
@@ -3,17 +3,18 @@
 import { useQuery } from "@tanstack/react-query";
 
 const getListings = async(): Promise<any> => {
-  const response = await fetch ("http://localhost:8000/api/vi/listings");
+  const response = await fetch ("http://127.0.0.1:8000/api/v1/listings/?limit=20&offset=0&sort=recent");
   return await response.json();
 };
 
-const { data } = useQuery({
-  queryKey: ["listings"],
-  queryFn: getListings,
-});
-
 export function Grid() {
-    return (
-        <div>{JSON.stringify(data)}</div>
-    );
+
+  const { data, refetch } = useQuery({
+    queryKey: ["listings"],
+    queryFn: getListings,
+  });
+
+  return (
+    <div>{JSON.stringify(data)}</div>
+  );
 };
